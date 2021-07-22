8b5947717307901691593bb1443bb5a83bd77804
AQUINRU <russel.aquino@oocl.com>
2021-07-22 15:12:09 +0800

    modified index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
-// TODO write your code here...
+document.write(name.toUpperCase());
 
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
-//TODO write your code here...
-
+const wordsArray = sentence.split(' '); //split by space
+for (var i = 0; i < wordsArray.length; i++) {
+    wordsArray[i] = wordsArray[i][0].toUpperCase() + wordsArray[i].substr(1);
+}
+document.write("<br>" + wordsArray.join(' ')); //join by space
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
-//TODO write your code here...
+document.write("<br>" + money.replace(/\D/g, ""));
